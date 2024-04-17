028fa985a04a42c9a58ec60a862603c74e59ea50
Yuki <github@risaton.net>
2024-04-17 14:47:40 +0900

    commit

--- src/main/java/net/risaton/yukimusicjava/Main.java
+++ src/main/java/net/risaton/yukimusicjava/Main.java
@@ -1,7 +1,32 @@
 package net.risaton.yukimusicjava;
 
-public class Main {
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+
+public class Main extends ListenerAdapter {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Dotenv dotenv = Dotenv.load();
+        String TOKEN = dotenv.get("DISCORD_TOKEN");
+        JDA jda = JDABuilder.createDefault(TOKEN)
+                .addEventListeners(new Main())
+                .setActivity(Activity.playing("Created by Yuki"))
+                .build();
+        jda.updateCommands().addCommands(
+                Commands.slash("ping", "Reply with Pong!")
+        ).queue();
+    }
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event){
+        switch (event.getName()){
+            case "ping":
+                event.reply("Pong!").queue();
+        }
     }
 }
