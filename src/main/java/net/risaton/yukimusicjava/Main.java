67ad8af472339d78b165def23f9a617b4dcb32da
Yuki <github@risaton.net>
2024-04-19 15:11:14 +0900

    commit

--- src/main/java/net/risaton/yukimusicjava/Main.java
+++ src/main/java/net/risaton/yukimusicjava/Main.java
@@ -4,14 +4,21 @@ import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
+import java.util.List;
+
+import static java.lang.System.out;
+
 public class Main extends ListenerAdapter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        out.println("Hello world!");
         Dotenv dotenv = Dotenv.load();
         String TOKEN = dotenv.get("DISCORD_TOKEN");
         JDA jda = JDABuilder.createDefault(TOKEN)
@@ -21,7 +28,9 @@ public class Main extends ListenerAdapter {
         jda.updateCommands().addCommands(
                 Commands.slash("ping", "Reply with Pong!")
         ).queue();
+        out.println("Logged in as: " + jda.getSelfUser().getName());
     }
+
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event){
         switch (event.getName()){
@@ -29,4 +38,5 @@ public class Main extends ListenerAdapter {
                 event.reply("Pong!").queue();
         }
     }
+
 }
